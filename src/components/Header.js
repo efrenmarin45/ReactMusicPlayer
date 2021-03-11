6408238c7e088afb27384a5e16700b44aed6a320
Efren_Marin <efren45marin@gmail.com>
2021-03-11 14:45:34 -0800

    Included Header and added material components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
+import HeadsetTwoToneIcon from '@material-ui/icons/HeadsetTwoTone';
+
+ const useStyles = makeStyles(theme => ({
+    title: {
+        marginLeft: theme.spacing(2)
+    }
+}));
 
 function Header(){
+    const classes = useStyles();
+
     return(
-        <div>Header</div>
+        <AppBar position='fixed' color='primary'>
+            <Toolbar>
+                <HeadsetTwoToneIcon />
+                <Typography className={classes.title} variant='h6' component='h1'>
+                    Music Player
+                </Typography>
+            </Toolbar>
+        </AppBar>
     )
 };
 
