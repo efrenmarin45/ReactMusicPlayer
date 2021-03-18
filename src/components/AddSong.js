ffaa4e5bbd3c55e2bb42850bd2c1612b1571304e
Efren_Marin <efren45marin@gmail.com>
2021-03-17 18:14:12 -0700

    Included text field along with an add button using Material UI

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,8 +1,30 @@
+import { InputAdornment, TextField, Button } from '@material-ui/core';
+import { AddBoxOutlined, Link } from '@material-ui/icons';
 import React from 'react';
 
 function AddSong(){
     return(
-        <div>Add Song</div>
+        <div>
+            <TextField 
+                placeholder="Add YouTube/Soundcloud URL"
+                fullWidth
+                margin="normal"
+                type="url"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment>
+                            <Link />
+                        </InputAdornment>
+                    )
+                }}
+            />
+            <Button 
+                variant="contained" 
+                color="primary"
+                endIcon={<AddBoxOutlined />}>
+                Add
+            </Button>
+        </div>
     )
 };
 
